4ae07a3e8f752778f64aa255605b551823c5cd58
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-12 15:23:00 +0200

    Update pushToDockerHub.groovy

--- vars/pushToDockerHub.groovy
+++ vars/pushToDockerHub.groovy
@@ -1,1 +1,11 @@
+def call(String imageName, String buildNumber, String credentialsId) {
+    withDockerRegistry([credentialsId: credentialsId]) {
+        sh """
+            echo "Pushing images to Docker Hub..."
+            docker push ${imageName}:${buildNumber}
+            docker tag ${imageName}:${buildNumber} ${imageName}:latest
+            docker push ${imageName}:latest
+        """
+    }
+}
 
