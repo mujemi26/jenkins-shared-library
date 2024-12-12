c36efc57fd5e256a38cd6f0acfd67849a1435a17
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-12 15:22:05 +0200

    Update validateEnvironment.groovy

--- vars/validateEnvironment.groovy
+++ vars/validateEnvironment.groovy
@@ -1,1 +1,11 @@
+def call() {
+    sh '''
+        echo "Checking required tools..."
+        docker version
+        kubectl version --client
+        kind version
 
+        echo "Checking cluster status..."
+        kind get clusters
+    '''
+}
