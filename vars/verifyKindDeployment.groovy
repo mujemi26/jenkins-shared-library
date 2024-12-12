6ad8d7c5632fc8db06fc5f583256b7d5095d20ed
Muhammad Gamal <59343923+mujemi26@users.noreply.github.com>
2024-12-12 15:23:31 +0200

    Update verifyKindDeployment.groovy

--- vars/verifyKindDeployment.groovy
+++ vars/verifyKindDeployment.groovy
@@ -1,1 +1,14 @@
+def call(String kubeConfig) {
+    withCredentials([file(credentialsId: kubeConfig, variable: 'KUBECONFIG_FILE')]) {
+        sh """
+            export KUBECONFIG=${KUBECONFIG_FILE}
+            kubectl get pods -l app=lab-app
+            for pod in \$(kubectl get pods -l app=lab-app -o name); do
+                kubectl logs \$pod
+            done
+            kubectl get service lab-app-service
+            curl -v http://localhost:30080 || true
+        """
+    }
+}
 
